eac7ade85b27950e9abe0dd3c06f766c1128e83d
administrator <manjuchandu555@bitbucket.org>
2018-10-10 18:29:59 +0530

    changes

---
+++ build/CMakeFiles/OPENCVAnnotation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPENCVAnnotation.dir/opencv_annotation.cpp.o"
+  "OPENCVAnnotation.pdb"
+  "OPENCVAnnotation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPENCVAnnotation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
